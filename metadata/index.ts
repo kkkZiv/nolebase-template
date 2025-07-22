640e2bc5bf1b949528acfdfeee8449d35d9a54f3
kkkZiv <2842515878@qq.com>
2025-07-22 13:11:27 +0800

    nameChange

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'kkkZiv'
-export const siteShortName = 'kkkZiv'
+export const siteName = 'Ziv'
+export const siteShortName = 'Ziv'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
